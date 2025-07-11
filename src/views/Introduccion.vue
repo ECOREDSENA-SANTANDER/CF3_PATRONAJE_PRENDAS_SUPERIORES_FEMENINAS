6924620e9e72d65b33ee3df837efa5db6ebec4ee
Andrea Botello <andrea.botellor@misena.edu.co>
2025-07-11 12:20:53 -0500

    VIdeo

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,7 +24,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/E0rddBJdX_4?si=FUoMFL2KZ3BYvPsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/USj_7P4U7ow?si=FUoMFL2KZ3BYvPsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         figcaption Video. Interpretación de modelos femeninos
 
 
